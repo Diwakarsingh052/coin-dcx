2ae7bc141f6b2bf5d2df39994e22992087c2a575
“Diwakar” <diwakarsingh052@gmail.com>
2023-02-27 14:47:08 +0530

    .

--- excercise/cmd/interface/interface-writer.go
+++ excercise/cmd/interface/interface-writer.go
@@ -1,1 +1,17 @@
-package _interface
+package main
+
+import "log"
+
+type user struct {
+	name  string
+	email string
+}
+
+func main() {
+	u := user{
+		name:  "john",
+		email: "john@email.com",
+	}
+
+	l := log.New(u, "custom log: ", log.LstdFlags) // make this line work
+}
