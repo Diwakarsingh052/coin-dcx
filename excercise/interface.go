c07dff4f51f5bb4e06eae1ab30aafe9daf54742b
“Diwakar” <diwakarsingh052@gmail.com>
2023-02-27 12:23:34 +0530

    .

--- excercise/interface.go
+++ excercise/interface.go
@@ -1,18 +1,6 @@
 package main
 
-import (
-	"coin-dcx/excercise/data"
-	"coin-dcx/excercise/data/stores/userdb"
-)
-
 func main() {
 	// call Create method of postgres using Storer interface
-	u := data.User{
-		Name:  "abc",
-		Email: "abc@email.com",
-	}
-	p := userdb.NewPostgres(nil)
-	s := data.NewStore(&p)
-	_ = s.Create(nil, u)
 
 }
