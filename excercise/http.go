866b30af0c25c18f739e8523583de433eacb0fef
“Diwakar” <diwakarsingh052@gmail.com>
2023-02-28 14:48:48 +0530

    .

---
+++ excercise/http.go
@@ -0,0 +1,36 @@
+package main
+
+import (
+	"context"
+	"fmt"
+	"log"
+	"net/http"
+)
+
+type HandlerFunc func(ctx context.Context, w http.ResponseWriter, r *http.Request) error
+
+var r *chi.Mux
+
+func main() {
+	r = chi.NewRouter()
+	HandleFunc("/home", Home)
+	panic(http.ListenAndServe(":8080", r))
+
+}
+
+func HandleFunc(pattern string, handler HandlerFunc) {
+	f := func(w http.ResponseWriter, r *http.Request) {
+		ctx := r.Context()
+		err := handler(ctx, w, r)
+		if err != nil {
+			log.Println(err)
+		}
+	}
+	r.HandleFunc(pattern, f)
+}
+
+func Home(ctx context.Context, w http.ResponseWriter, r *http.Request) error {
+	w.Write([]byte("hello this is our home page"))
+	fmt.Println("Home page invoked")
+	return nil
+}
