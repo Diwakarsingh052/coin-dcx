e3e4831f44d37f1499baad8e062c85aa326cbee6
“Diwakar” <diwakarsingh052@gmail.com>
2023-04-06 13:16:41 +0530

    .

--- exercise/database/db.go
+++ exercise/database/db.go
@@ -13,15 +13,15 @@ var ErrConn = errors.New("please provide a valid connection")
 
 func NewConfig(host, port, conn string) (*Config, error) {
 	if host == "" || port == "" || conn == "" {
-		return nil, ErrConn
+
 		// without pointer // Config{},ErrCon
 
 	}
 	db := host + port + conn
 	c := Config{db: db}
-	return &c, nil
 
 }
+
 func (c Config) AccessToDb() {
 
 	fmt.Println("accessing the data from the ", c.db)
