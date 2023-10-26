635312c90698509729bb39a39329061a1a8b285f
Diwakar Singh <46519302+Diwakarsingh052@users.noreply.github.com>
2023-10-26 09:22:11 +0530

    new

---
+++ exercise/interface-3/main.go
@@ -0,0 +1,12 @@
+package main
+
+func main() {
+
+	//final outcome:- call the Create method of Conn struct using Storer interface
+
+	// call mysql.New
+	// call postgres.New
+	//call stores.NewService with mysql and postgres
+	//call create method using store variable
+
+}
